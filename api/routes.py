7ab35b55f7688c6ce85eb05cc5a61bb413a803c3
Niewidzialny84 <niewidzialny84@vp.pl>
2020-09-10 19:08:51 +0200

    connection command

--- api/routes.py
+++ api/routes.py
@@ -4,8 +4,8 @@ import subprocess
 import os
 from flask import render_template, jsonify, make_response, abort, request
 
-tempcmd = '/opt/vc/bin/vcgencmd measure_temp'
-conncmd = ''
+tempcmd = "/opt/vc/bin/vcgencmd measure_temp | egrep temp"
+conncmd = "sudo  iwlist wlan0 scanning | egrep 'Cell |ESSID|Quality'"
 
 @app.errorhandler(500)
 def crashed(error):
@@ -16,7 +16,7 @@ def crashed(error):
 def index():
     if os.name == 'nt':
         return make_response('Wrong operating system',500)
-        
+
     temp = subprocess.check_output(tempcmd)
     conn = subprocess.check_output(conncmd)
     return render_template('index.html', temperature=temp.decode('utf-8'), connection=conn.decode('utf-8'))
